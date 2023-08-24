88ee27b4574c0924ff3f5c7272ae7919b9efb172
YashrajDodia <yashrajdodiya2308@gmail.com>
2023-08-24 19:56:37 +0530

    changes

--- Hellocoders2.cpp
+++ Hellocoders2.cpp
@@ -3,8 +3,30 @@ using namespace std;
 
 
 int main(){
-if(cout<<"Hello World"){}
+   int n;
+   cout<<"enter a number : ";
+   cin>>n;                                    // 34 56 32 54 72 3 9 8
 
-// cout<<"hello yashraj"<<endl;
+   int arr[n];
+   for(int i=0; i<n; i++){
+      cin>>arr[i];
+   }
+
+   for(int i=0; i<n-1; i++){
+      for(int j=i+1; j<n; j++){
+         if(arr[j]<arr[i]){
+            int temp;
+         temp = arr[j];
+         arr[j]=arr[i];
+         arr[i]=temp;
+         }
+      }
+   }
+
+   for(int i=0; i<n; i++){
+      cout<<arr[i]<<" ";
+   }
+
+   
    return 0;
 }
